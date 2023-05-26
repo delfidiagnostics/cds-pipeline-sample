591dcc8d92dfd258846410fe418cef10bb9180cd
Tony Wu <wu@delfidiagnostics.com>
2023-05-26 13:14:11 -0700

    add profiles
    add python script to download profiles.yml

--- cds_pipeline_sample/__init__.py
+++ cds_pipeline_sample/__init__.py
@@ -1,40 +1,14 @@
 import os
-
-import boto3
 from dagster_dbt import DbtCliClientResource
 from dagster import (
     ScheduleDefinition,
-    graph,
     define_asset_job,
     Definitions,
     load_assets_from_modules,
-    op,
 )
 
 from cds_pipeline_sample import assets
 from cds_pipeline_sample.assets import DBT_PROFILES, DBT_PROJECT_PATH
-
-
-# op definition for downloading profiles.yml
-@op
-def download_profiles_file_from_s3(context):
-    bucket = "delfi-clindev"
-    s3_file = "cds-pipeline-data/dev/profiles.yml"
-    local_file = DBT_PROFILES + "/profiles.yml"
-    s3 = boto3.client("s3")
-    s3.download_file(bucket, s3_file, local_file)
-    context.log.info(f"Downloaded {s3_file} from S3 to {local_file}")
-
-
-# op definition for delete profiles.yml
-@op
-def delete_profiles_file(context):
-    local_file = DBT_PROFILES + "/profiles.yml"
-    if os.path.isfile(local_file):
-        os.remove(local_file)
-
-    context.log.info(f"Deleted {local_file}")
-
 
 # define resources
 resources = {
@@ -44,10 +18,8 @@ resources = {
     ),
 }
 
-
 # define jobs
 run_everything_job = define_asset_job("run_everything", selection="*")
-
 
 defs = Definitions(
     assets=load_assets_from_modules([assets]),
@@ -60,7 +32,8 @@ defs = Definitions(
     resources=resources,
 )
 
-# If you want to debug locally without running a dagit instance, you can uncomment the following.
-# This runs the load_all_job job which runs all assets.
+# if you want to debug locally without running a dagit instance, you can uncomment the following
 if __name__ == "__main__":
     _result = defs.get_job_def("run_everything").execute_in_process()
+    local_file = DBT_PROFILES + "/profiles.yml"
+    os.remove(local_file)
--- cds_pipeline_sample/assets/__init__.py
+++ cds_pipeline_sample/assets/__init__.py
@@ -1,11 +1,18 @@
 from dagster_dbt import load_assets_from_dbt_project
-
 from dagster import file_relative_path
-
+import boto3
 
 DBT_PROJECT_PATH = file_relative_path(__file__, "../../surf_l201")
 DBT_PROFILES = file_relative_path(__file__, "../../surf_l201")
 
+# download profiles.yml
+bucket = "delfi-clindev"
+s3_file = "cds-pipeline-data/dev/profiles.yml"
+local_file = DBT_PROFILES + "/profiles.yml"
+s3 = boto3.client("s3")
+s3.download_file(bucket, s3_file, local_file)
+
+# define dbt assets
 dbt_assets = load_assets_from_dbt_project(
-    project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["surf_l201"]
+    project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["staging"]
 )
