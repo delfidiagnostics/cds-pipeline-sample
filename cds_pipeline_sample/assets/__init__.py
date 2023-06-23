1841d3df15457a3dc0d9abd75f5b8287fed05007
Tony Wu <wu@delfidiagnostics.com>
2023-06-23 10:16:54 -0700

    fix docker build
    fix docker build

--- cds_pipeline_sample/assets/__init__.py
+++ cds_pipeline_sample/assets/__init__.py
@@ -15,14 +15,6 @@ if ENV:
     environ["AWS_ACCESS_KEY_ID"] = AWS_KEYS["AWS_ACCESS_KEY_ID"]
     environ["AWS_SECRET_ACCESS_KEY"] = AWS_KEYS["AWS_SECRET_ACCESS_KEY"]
 
-
-# download profiles.yml
-bucket = "delfi-clindev"
-s3_file = "cds-pipeline-data/dev/profiles.yml"
-local_file = DBT_PROFILES + "/profiles.yml"
-s3 = boto3.client("s3")
-s3.download_file(bucket, s3_file, local_file)
-
 # define dbt assets
 dbt_assets = load_assets_from_dbt_project(  
     project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["staging"], node_info_to_group_fn=lambda k: "cds_pipeline_sample"
